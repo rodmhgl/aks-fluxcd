466dbb1537684626af35b345c11bfdece5aeac55
Rod Stewart <rodstewart@gmail.com>
2023-10-10 08:36:26 -0500

    Removes fluff

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,6 @@ output "rg_name" {
   value = azurerm_resource_group.this.name
 }
 
-output "istio_ingress_ip" {
-  value = data.kubernetes_service.this.status.0.load_balancer.0.ingress.0.ip
-}
-
 output "get_creds" {
   value = "az aks get-credentials --resource-group ${azurerm_resource_group.this.name} --name ${azurerm_kubernetes_cluster.this.name} --admin"
 }
